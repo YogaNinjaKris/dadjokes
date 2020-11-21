b22e7be890e55c39745e0acee592f76a203feac1
Kristyna Nedvedova <nedvedovakristyna@gmail.com>
2020-11-21 15:08:20 +0100

    První commit test

---
+++ OneDrive/Czechitas/react-starter/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.html';
+
+render(
+  <>
+    <header>
+      <h1>Dadjokes test funguju</h1>
+    </header>
+    <main>
+      <p>React stránka</p>
+    </main>
+    <footer>Kristýna</footer>
+  </>,
+  document.querySelector('#app'),
+);
